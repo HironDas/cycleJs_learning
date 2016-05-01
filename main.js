06e441a443b5a312bb49e585867775e1f20e97bb
Hiron Das <hiron_cse@yahoo.com>
2016-05-01 21:51:08 +0600

    in lesson 03

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-Rx.Observable.timer(0, 100)
-	.map(i => 'Second elapsed ${i}'+i)
-	.subscribe(text => {
+function main(){
+	return {
+		DOM: Rx.Observable.timer(0, 100)
+			.map(i => 'Second elapsed ${i}'),
+		Log: Rx.Observable.timer(0, 2000)
+				.map(i=> 2*i)
+		};
+}
+
+function DOMDriver(text$){
+	text$.subscribe(text => {
 		const container = document.querySelector('#app');
 		container.textContent = text;
 	});
+}
+
+function consoleLogDriver(msg$){
+	msg$.subscribe(msg=> console.log(msg));
+}
+
+function run(mainFn, effects){
+	const sink = mainFn();
+	Object.keys(effects).forEach(key=> {
+		effects[key](sink[key]);
+	});
+	//DOMEffect(sink.DOM);
+	//consoleLogEffect(sink.Log);
+}
+
+const drivers = {
+	DOM: DOMDriver,
+	Log: consoleLogDriver
+}
+
+run(main, drivers);
+

e620c603da2b93c39480e597bc4ed04bc2b8cbda
Hiron Das <hcdas.09@gmail.com>
2016-05-02 02:09:49 +0600

    some probs with code in lecture video 6

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
-function main(){
-	return {
-		DOM: Rx.Observable.timer(0, 100)
-			.map(i => 'Second elapsed ${i}'),
+function main(source){
+	const click$ = source.DOM;
+
+	const sinks = {
+		DOM: click$.startWith(null)
+			.flatMapLatest(()=>{
+				Rx.Observable.timer(0, 1000)
+					.map(i => `Second elapsed ${i}`)
+			}),
 		Log: Rx.Observable.timer(0, 2000)
 				.map(i=> 2*i)
 		};
+	return sinks;
 }
+
+//source : input (read) effects
+//sink : output (write) effects
 
 function DOMDriver(text$){
 	text$.subscribe(text => {
 		const container = document.querySelector('#app');
 		container.textContent = text;
 	});
+	
+	const DOMSource = Rx.observable.fromEvent(document, 'click');
+	return DOMSource;
 }
 
 function consoleLogDriver(msg$){
 	msg$.subscribe(msg=> console.log(msg));
 }
 
-function run(mainFn, effects){
-	const sink = mainFn();
-	Object.keys(effects).forEach(key=> {
-		effects[key](sink[key]);
+function run(mainFn, drivers){
+	const proxySources = {};
+	/*const DOMSource = drivers.DOM(sink.DOM);
+	DOMSource.subscribe(click => {
+		proxyDOMSource.onNext(click); 
+	})*/
+	Object.keys(drivers).forEach(key=> {
+		proxySources[key] = new Rx.Subject();
 	});
-	//DOMEffect(sink.DOM);
-	//consoleLogEffect(sink.Log);
+
+	const sinks = mainFn(proxySources);
+
+	Object.keys(drivers).forEach(key=> {
+		const source = drivers[key](sinks[key]);
+		source.subscribe(x => proxySources[key].onNext(x));
+	});
 }
 
 const drivers = {

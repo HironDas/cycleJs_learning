6bf05640e3b49fedaee2c4ab11028c0b93aa3a8d
Hiron Das <hcdas.09@gmail.com>
2016-05-08 12:09:59 +0600

    lesseon is finished

--- js/bmi_standard.js
+++ js/bmi_standard.js
@@ -33,14 +33,19 @@ function view(state$){
 }
 
 
-function labeledSlider(sources) {
+function LabeledSlider(sources) {
 	const changes$ = intent(sources.DOM);
 	const state$ = model(changes$, sources.props);
 	return{
-		DOM: view(state$)
+		DOM: view(state$),
+		value: state$.map(state => state.value)
 	}
 	
 }
+
+function IsolatedLabeledSlidser(sources){
+	return isolate(LabeledSlider)(sources);
+};
 
 function main(sources){
 	const weightProps$ = Rx.Observable.of({
@@ -49,36 +54,38 @@ function main(sources){
 		min: 40,
 		max: 150,
 		init: 70
-	})
-
+	});
 	const heightProps$ = Rx.Observable.of({
 		label: 'Height',
 		unit: 'cm',
 		min: 140,
 		max: 220,
 		init: 170
+	});
+
+	// const weightSlider = isolate(LabeledSlider);
+	var weightSinks = IsolatedLabeledSlidser({ DOM: sources.DOM, props: weightProps$});
+	const  weightVTree$ = weightSinks.DOM;
+	const weightValue$ = weightSinks.value;
+
+
+	// const heightSlider = isolate(LabeledSlider);
+	var heightSinks = IsolatedLabeledSlidser({ DOM: sources.DOM, props: heightProps$});
+	const heightVTree$ = heightSinks.DOM;
+	const heightValue$ = heightSinks.value;
+
+	const bmi$ = Rx.Observable.combineLatest(weightValue$, heightValue$, (width, height) => {
+		const heightMeters = height * 0.01;
+		const bmi = Math.round(width / (heightMeters * heightMeters));
+		return bmi;
 	})
-	var weightSinks = labeledSlider({ DOM: sources.DOM.select('.weight'), props: weightProps$});
-
-	const  weightVTree = weightSinks.DOM.map(vtree => {
-			vtree.properties.className = 'weight';
-			return vtree;
-		}
-	)
-
-	var heightSinks = labeledSlider({ DOM: sources.DOM.select('.height'), props: heightProps$});
-
-	const  heightVTree = heightSinks.DOM.map(vtree => {
-			vtree.properties.className = 'height';
-			return vtree;
-		}
-	)
 
 	const vtree$ = Rx.Observable.combineLatest(
-		weightVTree, heightVTree, (weightVTree, heightVtee) =>
+		bmi$, weightVTree$, heightVTree$, (bmi, weightVTree, heightVtee) =>
 			div([
 				weightVTree,
-				heightVtee
+				heightVtee,
+				h2('BMI is '+bmi)
 			])
 		);
 	return { DOM: vtree$};
